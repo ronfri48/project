e076576a4524f9eb3318b1007cc0216f7dbc2c01
ronfri48 <44091829+ronfri48@users.noreply.github.com>
2022-02-25 16:44:17 +0200

    Add ttl thread

--- Scripts/server_main.py
+++ Scripts/server_main.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from update_mud_configs import MUD
+import time, traceback
+import threading
+
+def every(delay, task):
+  next_time = time.time() + delay
+  while True:
+    time.sleep(max(0, next_time - time.time()))
+    try:
+      task()
+    except Exception:
+      traceback.print_exc()
+      # in production code you might want to have this instead of course:
+      # logger.exception("Problem while executing repetitive task.")
+    # skip tasks if we are behind schedule:
+    next_time += (time.time() - next_time) // delay * delay + delay
 
 app = Flask(__name__)
 
-ONE_MIN = 1000 * 60
+ONE_HOUE = 1000 * 60 * 60
 
-mud = MUD(ONE_MIN)
+mud = MUD(ONE_HOUE)
+
+
+threading.Thread(target=lambda: every(300, mud.handle_invalidate_request)).start()
 
 @app.route('/update-mud', methods=['POST'])
 def update_mud():
